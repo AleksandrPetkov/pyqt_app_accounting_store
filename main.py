48a3ec1b431f2bfa1825f1987e38f8db9d8e3936
AleksandrPetkov <109108541+AleksandrPetkov@users.noreply.github.com>
2023-04-22 17:29:31 +0300

    add media folder and icon

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import sys
 
 from PyQt5 import QtWidgets, QtGui
 from app import AppShop
+import os
+
+basedir = os.path.dirname(__file__)
+
+try:
+    from ctypes import windll  # Only exists on Windows.
+    myappid = 'mycompany.myproduct.subproduct.version'
+    windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+except ImportError:
+    pass
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    app.setStyle('Breeze')
     app.setWindowIcon(QtGui.QIcon('ico.png'))
+    app.setWindowIcon(QtGui.QIcon(os.path.join(basedir, 'media', 'ico.png')))
+    app.setStyle('Breeze')
     window = AppShop()  # Создаём объект класса ExampleApp
-    window.setWindowIcon(QtGui.QIcon('ico.png'))
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
 
 if __name__ == '__main__':
     main()
+
+
+# pyinstaller main.spec
